55b4bdfe3c867bfd175db5bdbc9153e67e2996db
howardbdev <howarddevennish@gmail.com>
2018-08-17 13:46:59 -0400

    add some method requests in Pet class

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,21 @@ class Pet < ActiveRecord::Base
   belongs_to :owner
   validates :name, :type_of_animal, presence: true
 
-  def self.all_pets_speak
-    all.each {|p| puts "Hello! I'm #{p.name} and I'm a #{p.breed} #{p.type_of_animal}, I'm #{p.age} years old, and I belong to #{p.owner.name}"}
-  end
+  # 1. Build a class method that returns all dogs
+
+
+  # 2. Build a class method that returns all cats
+
+
+  # 3. Build a class method that returns all pets of a particular breed.  It should take an argument, a string that represents the breed
+
+
+  # 4. Build a class method that takes an argument of a breed, but `puts` a message for every matching pet that talks about itself, such as:
+  #  Hello! I'm Poppy, I'm 9 years old, and I belong to Walden Macnair
+
+
+  # 5. BONUS - build a finder method that takes a key/value pair.  The key represents an attribute and the value is its value.
+  #  It should return any pets that match that attribute/value pair.  Example:
+  #  Pet.find_all_by_attribute(name: "Garfield") would return all pets named Garfield.
+
 end
